cc9f536ae7f59a97f8598adf466e5d5393f6391b
Gio Alfonso <gioalfonso14@gmail.com>
2020-09-27 21:16:48 -0700

    finish 'Working with Classes'

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,26 +1,52 @@
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
-const item2 = document.querySelector('.item2');
-const item2Image = item2.querySelector('img');
-const heading = document.querySelector('h2');
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.remove('cool');
+pic.classList.toggle('round');
+console.log(pic.classList);
+
+// try with event listener
+
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
 
 
-console.log(heading.textContent);
-console.log(heading.innerText);
-// set the textContent Property on that element
-// heading.textContent = 'Wes is cool';
+
+
+
+
+
+
+
+
+
+
+
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const item2 = document.querySelector('.item2');
+// const item2Image = item2.querySelector('img');
+// const heading = document.querySelector('h2');
+
+
 // console.log(heading.textContent);
 // console.log(heading.innerText);
+// // set the textContent Property on that element
+// // heading.textContent = 'Wes is cool';
+// // console.log(heading.textContent);
+// // console.log(heading.innerText);
 
 
-console.log(heading.innerHTML);
-console.log(heading.outerHTML);
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
 
-// pizzaList.textContent = `${pizzaList.textContent} 🍕`
-pizzaList.insertAdjacentText('afterbegin', '🍕');
-pizzaList.insertAdjacentText('beforeend', '🍕');
+// // pizzaList.textContent = `${pizzaList.textContent} 🍕`
+// pizzaList.insertAdjacentText('afterbegin', '🍕');
+// pizzaList.insertAdjacentText('beforeend', '🍕');
 
 
