2ae24df566b4cfd526a139f90890c6d38042e87d
Gio Alfonso <gioalfonso14@gmail.com>
2020-10-08 21:32:38 -0700

    creating HTML in JS

---
+++ exercises/20 - The DOM/creating.js
@@ -0,0 +1,8 @@
+console.log("it works");
+
+const myParagraph = document.createElement('p');
+myParagraph.textContent = 'I am a P';
+myParagraph.classList.add = ('special');
+console.log(myParagraph);
+
+// continue @3:30
