20cf2e621c07a67c7700c8099089103fcd11b7c5
Giovanni Alfonso <gioalfonso14@gmail.com>
2020-10-30 16:47:17 -0700

    finish Event Listener

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -9,5 +9,27 @@ butts.addEventListener('click', handleClick);
 coolButton.addEventListener('click', handleClick);
 
 //stop an event listener
-
 // butts.removeEventListener('click', handleClick);
+
+// LISTEN ON MULTIPLE ITEMS
+const buyButtons = document.querySelectorAll('button.buy');
+
+// console.log(buyButtons);
+
+function buyItem() {  // handler
+  console.log('BUYING ITEM');
+}
+
+function handleBuyButtonClick(buyButton) {
+  console.log('binding the buy button');
+  buyButton.addEventListener('click', buyItem);
+}
+
+buyButtons.forEach(handleBuyButtonClick);
+
+//using arrow function
+buyButtons.forEach((button) =>{
+  button.addEventListener('click', () => {
+    console.log('YOU CLICKED IT');
+  });
+});
