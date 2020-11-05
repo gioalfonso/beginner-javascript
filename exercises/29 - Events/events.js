3110c7807098a0bd42f7301d2e54c1edf8a670ce
Giovanni Alfonso <gioalfonso14@gmail.com>
2020-11-04 17:01:34 -0800

    Events = targets, bubbling... stop @14:00

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -15,16 +15,35 @@ butts.removeEventListener('click', handleClick);
 const buyButtons = document.querySelectorAll('button.buy');
 
 function handleBuyButtonClick(event) {
-  console.log('You are buying it!');
-  console.log(event)
-}
+  // console.log(parseFloat(event.target.dataset.price))
+  
+  // or you can do this way also
+  const button = event.target;
+  // console.log(button.textContent);
 
+  // or
+  console.log(event.currentTarget);
+  // console.log(event.target)
+  // console.log(event.target === event.currentTarget)
+  
+  // Stop this event from bubbling UP
+  event.stopPropagation();
+}
 
 buyButtons.forEach(function(buyButton) {
   buyButton.addEventListener('click', handleBuyButtonClick)
 })
 
-// stopped at 04:06
+
+
+window.addEventListener('click', function(event) {
+  console.log('You Clicked The Window');
+  console.log(event.target)
+  console.log(event.currentTarget)
+});
+
+// stop @14:00
+
 
 
 
