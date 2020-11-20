bee1a8e0343585a16753b28e771f45c28e9ec6fc
Giovanni Alfonso <gioalfonso14@gmail.com>
2020-11-19 17:07:58 -0800

    stop @37:10 Etch-a-sketch

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -2,7 +2,7 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
-
+const MOVE_AMOUNT = 40;
 // Setup our canvas for drawing
 // make a variable called height and width from the same properties on our canvas
 const { width, height} = canvas; 
@@ -12,11 +12,12 @@ const { width, height} = canvas;
 // create randon x and y starting points on the canvas.
 let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
-// ======================= stopped @12:41 etch sketch
 
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUNT;
 
 ctx.beginPath(); // start the drawing
 ctx.moveTo(x, y);
@@ -24,20 +25,47 @@ ctx.lineTo(x, y);
 ctx.stroke();
 
 // Write a draw function
-function draw(options) {
-  console.log(options);
+function draw({key}) {      // using destructuring here (object)
+  // increment the hue
+  hue = hue + 10;
+  console.log(hue);
+  ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  console.log(key);
+  //start the path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  // move our x and y values depending on what the user did
+  switch( key ) {
+    case 'ArrowUp':
+      y = y - MOVE_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x = x + MOVE_AMOUNT;
+      break;
+    case 'ArrowDown':
+      y = y + MOVE_AMOUNT;
+      break;
+    case 'ArrowLeft':
+      x = x - MOVE_AMOUNT;
+      break;
+    
+      default:
+      break;
+  }
+  
+  ctx.lineTo(x, y);
+  ctx.stroke();
 }
+
 
 // Write a handler for the keys
 function handleKey(e) {
   if(e.key.includes('Arrow')) {
     e.preventDefault();
     draw({ key: e.key})
-    console.log(e.key)
-    console.log('HANDLING KEY');
   }
 }
-// Clear / shake function
+// Clear shake function
 
 // listen for arrow keys
 window.addEventListener('keydown', handleKey);
