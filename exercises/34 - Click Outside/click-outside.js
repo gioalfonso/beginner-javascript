99925f5256883a598a4b5ed15bf8a1394f0462ce
Giovanni Alfonso <gioalfonso14@gmail.com>
2020-11-20 17:13:52 -0800

    stop @ 09:30 - Click Outside Modal

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,7 +1,7 @@
 const cardButtons = document.querySelectorAll('.card button');
 
 function handleCardButtonClick() {
-  console.log('YOU CLICKED IT');
+  console.log('YA CLICKED IT');
 }
 
 cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick))
